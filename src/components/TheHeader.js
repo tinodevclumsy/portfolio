6417f894e33e498e321a8a9a3f7f8d21c01b2b6a
Seunngjun Lee <seunngjunlee@Seunngjuns-MacBook-Air.local>
2023-05-25 19:00:05 +0900

    create sticky header

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -1,40 +1,36 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import logo from "../assets/images/logo.png"
 
 export default function Header() {
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop)
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", updateScroll)
+  })
+
   return (
-    <header className="header fixed top-0 left-0 w-full">
+    <header className={scrollPosition > 0 ? "header--sticky header fixed top-0 left-0 w-full" : "header fixed top-0 left-0 w-full"}>
       <div className="container flex justify-between items-center">
         <div className="container--image container--logo">
           <img src={logo} alt="Logo" />
         </div>
 
         <ul className="menu--list-top flex">
-          <li
-            className="menu--item inline-block mx-3.5"
-            href="#about"
-          >
+          <li className="menu--item inline-block mx-3.5" href="#about">
             <a className="py-5">About</a>
           </li>
 
-          <li
-            className="menu--item inline-block mx-3.5"
-            href="#work"
-          >
+          <li className="menu--item inline-block mx-3.5" href="#work">
             <a className="py-5">Work</a>
           </li>
 
-          <li
-            className="menu--item inline-block mx-3.5"
-            href="#project"
-          >
+          <li className="menu--item inline-block mx-3.5" href="#project">
             <a className="py-5">Project</a>
           </li>
 
-          <li
-            className="menu--item inline-block mx-3.5"
-            href="#contact"
-          >
+          <li className="menu--item inline-block mx-3.5" href="#contact">
             <a className="py-5">Contact</a>
           </li>
           {/* <a
